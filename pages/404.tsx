83be1b924160da7b9da56171e8a43d3223de98aa
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-05-03 04:09:37 -0800

    added custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Hero from '../components/errorPage/Hero'
 
 function index() {
-  return <div>index</div>
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
 }
 
 export default index
